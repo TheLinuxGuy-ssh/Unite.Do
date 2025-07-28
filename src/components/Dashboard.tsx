8001f3dd2792a9c6a54157c9ba24be7e365d6853
TheLinuxGuy <107614947+TheLinuxGuy-ssh@users.noreply.github.com>
2025-07-28 10:40:52 +0000

    dashboard updated

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,16 @@ const Dashboard = () => {
         <>
         <div className="dashboard w-full rounded-4xl m-4 ml-0">
         <div className="dashboard-header">
-            <h1 className="text-4xl font-bold">Welcome to Unite.Do</h1>
+            <h1 className="text-4xl font-bold">Welcome to <br /> Unite.<span className="text-[#fecf3e]">Do</span></h1>
+        </div>
+        <div className="dashboard-content">
+            <div className="flex flex-1 m-2 bg-white rounded-4xl">
+                
+            </div>
+            <div className="flex flex-1 m-2">
+                
+            </div>
+
         </div>
         </div>
         </>
